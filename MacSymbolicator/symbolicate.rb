c4582b38a8224fbdf1a52a2eee7836cb91a8aa01
inket <injekter@gmail.com>
2015-09-23 14:13:24 +0900

    Fixed indentation

--- MacSymbolicator/symbolicate.rb
+++ MacSymbolicator/symbolicate.rb
@@ -3,22 +3,22 @@ dsym_file = ARGV[1]
 quiet = !ARGV[2].nil? && ARGV[2] == "-q"
 
 if (crash_file.nil? || dsym_file.nil?)
-	puts "Usage: ruby symbolicate.rb <crash_file> <dsym_file>"
-	exit false
+  puts "Usage: ruby symbolicate.rb <crash_file> <dsym_file>"
+  exit false
 end
 
 if (dsym_file =~ /\.dsym\/?/i)
-	Dir.entries(dsym_file+"/Contents/Resources/DWARF/").each {
-		|entry|
-		next if entry.start_with?(".")
-		dsym_file = dsym_file+"/Contents/Resources/DWARF/"+entry
-		break
-	}
+  Dir.entries(dsym_file+"/Contents/Resources/DWARF/").each {
+    |entry|
+    next if entry.start_with?(".")
+    dsym_file = dsym_file+"/Contents/Resources/DWARF/"+entry
+    break
+  }
 end
 
 crash_content = ""
 File.open(crash_file, "r") {
-	|f| crash_content = f.read
+  |f| crash_content = f.read
 }
 
 process_name = crash_content.scan(/^Process:\s+(.*?)\s\[/im).flatten.first
@@ -30,32 +30,32 @@ code_types_to_arch = {'X86-64' => 'x86_64', 'X86' => 'i386', 'PPC' => 'ppc'}
 arch = code_types_to_arch[code_type] || code_type
 
 if (!quiet)
-	puts "Process: #{process_name}"
-	puts "Bundle Identifier: #{bundle_identifier}"
-	puts "Load address: #{load_address}"
+  puts "Process: #{process_name}"
+  puts "Bundle Identifier: #{bundle_identifier}"
+  puts "Load address: #{load_address}"
 end
 
 result = `xcrun atos -o \"#{dsym_file}\" -arch #{arch} -l #{load_address} #{addresses.join(" ")}`.strip
 
 if (!result.empty?)
-	lines = result.split("\n")
-	
-	if lines.count != addresses.count
-		puts "Unexpected error."
-		puts lines
-		exit false
-	else
-		addresses.each_index {
-			|index|
-			symbol = lines[index]
-			crash_content.gsub!(/#{addresses[index]}.*?$/i, "#{addresses[index]} #{symbol}")
-		}
-		
-		puts crash_content
-                                                     
-		exit true
-	end
+  lines = result.split("\n")
+
+  if lines.count != addresses.count
+    puts "Unexpected error."
+    puts lines
+    exit false
+  else
+    addresses.each_index {
+      |index|
+      symbol = lines[index]
+      crash_content.gsub!(/#{addresses[index]}.*?$/i, "#{addresses[index]} #{symbol}")
+    }
+
+    puts crash_content
+
+    exit true
+  end
 else
-	puts "Couldn't continue because of atos error."
-	exit false
+  puts "Couldn't continue because of atos error."
+  exit false
 end
