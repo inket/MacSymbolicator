b1f16ab70a368da3f5e4690dec0745e76622e18f
Mahdi Bchatnia <git@mahdi.jp>
2024-08-27 14:05:53 +0900

    Improve IPS translator comment

--- MacSymbolicator/Controllers/Translator.swift
+++ MacSymbolicator/Controllers/Translator.swift
@@ -15,9 +15,10 @@ public enum Translator {
 
     /// Uses the private system framework OSAnalytics to perform the translation of the IPS crash report into
     /// the old-format crash report, the same way Console.app translates it.
-    /// This is "hacky", but because we don't have any documentation for the IPS format, it's faster and less
-    /// error-prone than manually parsing/translating IPS files ourselves. Obvious downside is that this
-    /// method/class/framework could change in the future, but let's deal with that when it happens.
+    /// This is "hacky", but it's faster to implement and less error-prone than parsing/translating IPS files ourselves.
+    /// That's of course assuming this method/class/framework doesn't change, but let's deal with that when it happens.
+    /// ** IPS format is well-documented here:
+    /// https://developer.apple.com/documentation/xcode/interpreting-the-json-format-of-a-crash-report
     public static func translatedCrash(forIPSAt path: URL) throws -> String {
         guard let bundle = Bundle(path: "/System/Library/PrivateFrameworks/OSAnalytics.framework") else {
             throw Error.couldNotLoadOSAnalytics
